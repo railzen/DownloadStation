c2592b1538d93e67125ef0d892681214b8906e2a
RailZen <railzen@126.com>
2024-04-20 00:27:09 +0800

    Update ludo.sh

--- Shell/cherry/ludo.sh
+++ Shell/cherry/ludo.sh
@@ -5227,7 +5227,7 @@ EOF
     cd ~
     curl -sS -O https://raw.githubusercontent.com/railzen/DownloadStation/main/Shell/cherry/ludo.sh && chmod +x ludo.sh
     echo ""
-    echo "Current Version:V1.0.1"
+    echo "Current Version:V1.0.0"
     echo "脚本已更新到最新版本！"
     break_end
     back_main
