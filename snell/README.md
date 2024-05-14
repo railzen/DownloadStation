963dfdf4f861d4965dffdd6caace5ce40bb58dd2
RailZen <railzen@126.com>
2024-05-14 21:42:17 +0800

    [ADD] Add snell script

---
+++ snell/README.md
@@ -0,0 +1,20 @@
+# Snell v4 Installation Script
+
+[Snell Protocol Official Manual](https://manual.nssurge.com/others/snell.html)
+## Install
+```bash
+bash -c "$(curl -sL https://raw.githubusercontent.com/railzen/DownloadStation/main/snell/snell.sh)"
+```
+or
+```bash
+bash -c "$(curl -sL https://mirror.ghproxy.com/https://raw.githubusercontent.com/railzen/DownloadStation/main/snell/snell.sh)"
+```
+## Uninstall
+```bash
+bash -c "$(curl -sL https://raw.githubusercontent.com/railzen/DownloadStation/main/snell/rmsnell.sh)"
+```
+or
+```bash
+bash -c "$(curl -sL https://mirror.ghproxy.com/https://raw.githubusercontent.com/railzen/DownloadStation/main/snell/rmsnell.sh)"
+```
+
---
+++ snell/snell.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+# Install dependencies based on the Linux distribution
+if cat /etc/*-release | grep -q -E -i "debian|ubuntu|armbian|deepin|mint"; then
+    sudo apt-get install wget unzip dpkg -y
+elif cat /etc/*-release | grep -q -E -i "centos|red hat|redhat"; then
+    sudo yum install wget unzip dpkg -y
+elif cat /etc/*-release | grep -q -E -i "arch|manjaro"; then
+    sudo pacman -S wget dpkg unzip --noconfirm
+elif cat /etc/*-release | grep -q -E -i "fedora"; then
+    sudo dnf install wget unzip dpkg -y
+fi
+
+
+# Download and install snell
+cd
+ARCH=$(uname -m)
+BASE_URL="https://dl.nssurge.com/snell/snell-server-v4.0.1-linux"
+case $ARCH in
+    "x86_64")
+        PACKAGE="${BASE_URL}-amd64.zip"
+        ;;
+    "i686" | "i386")
+        PACKAGE="${BASE_URL}-i386.zip"
+        ;;
+    "aarch64")
+        PACKAGE="${BASE_URL}-aarch64.zip"
+        ;;
+    "armv7l")
+        PACKAGE="${BASE_URL}-armv7l.zip"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+wget $PACKAGE
+
+if [ $? -ne 0 ]; then
+    echo "Download failed!"
+    exit 1
+fi
+unzip -o ${PACKAGE##*/} 
+
+# Create systemd service
+echo -e "[Unit]\nDescription=snell server\n[Service]\nUser=$(whoami)\nWorkingDirectory=$HOME\nExecStart=$HOME/snell-server\nRestart=always\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/snell.service > /dev/null
+echo "y" | sudo ./snell-server
+sudo systemctl start snell
+sudo systemctl enable snell
+
+ufw allow $(cat snell-server.conf | grep -i listen | cut --delimiter=':' -f2)
+
+# print snell server info
+echo
+echo "Copy the following line to Surge, under the [Proxy] section:" 
+echo "$(curl -s ipinfo.io/city) = snell, $(curl -s ipinfo.io/ip), $(cat snell-server.conf | grep -i listen | cut --delimiter=':' -f2), psk=$(grep 'psk' snell-server.conf | cut -d= -f2 | tr -d ' '), version=4, tfo=true"
