cfec0e72903a986b64b3d2449e1116736054226f
RailZen <railzen@126.com>
2024-05-19 17:11:42 +0800

    Update Snell_v2.sh

--- Shell/snell/Snell_v2.sh
+++ Shell/snell/Snell_v2.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 
-sh_ver="1.0.0 build240519"
+sh_ver="1.0.0 build240521"
 filepath=$(cd "$(dirname "$0")"; pwd)
 file_1=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
 FOLDER="/etc/snell/"
