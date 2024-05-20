586ea1cfe1dce981e6b28e1b4453e6b287f10485
RailZen <railzen@126.com>
2024-05-21 05:47:39 +0800

    Update ludo.sh

--- Shell/cherry/ludo.sh
+++ Shell/cherry/ludo.sh
@@ -11,7 +11,7 @@ Blue='\033[0;34m'
 Red='\033[31m'
 Gray='\e[37m'
 
-main_version="V1.0.7.2058 Build240521"
+main_version="V1.0.7.2059 Build240521"
 
 main_menu_start() {
 while true; do
@@ -3663,11 +3663,11 @@ Update_Shell(){
 		read -p "发现新版本 ${sh_new_ver} ，是否更新？[Y/n]" yn
 		[[ -z "${yn}" ]] && yn="y"
 		if [[ ${yn} == [Yy] ]]; then
-            cd /opt/cherry_script
+            cd /opt/cherry_script/
             curl -sS -O https://raw.githubusercontent.com/railzen/DownloadStation/main/Shell/cherry/ludo.sh && chmod +x ludo.sh
             rm -f /usr/local/bin/ludo
-            cp -f ./ludo.sh /opt/cherry_script/ludo.sh > /dev/null 2>&1
             cp -f /opt/cherry_script/ludo.sh /usr/local/bin/ludo > /dev/null 2>&1
+            cp -f /opt/cherry_script/ludo.sh -/ludo.sh > /dev/null 2>&1
             echo ""
 			echo -e "脚本已更新为最新版本 ${sh_new_ver} ! "
             break_end
