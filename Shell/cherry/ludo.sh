1c44f31351920f9e7142f23c23fb2f7453d748f7
RailZen <railzen@126.com>
2024-06-09 20:55:44 +0800

    Update ludo.sh

--- Shell/cherry/ludo.sh
+++ Shell/cherry/ludo.sh
@@ -3720,7 +3720,7 @@ done
 
 Update_Shell(){
 	echo -e "当前版本为 ${main_version} ，开始检测最新版本..."
-	sh_new_ver=$(wget --no-check-certificate -qO- "https://raw.githubusercontent.com/railzen/DownloadStation/main/Shell/cherry/ludo.sh"|grep 'main_version="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
+	sh_new_ver=$(curl -s "https://raw.githubusercontent.com/railzen/DownloadStation/main/Shell/cherry/ludo.sh"|grep 'main_version="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
 	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 检测最新版本失败 !" && break_end
 	if [[ ${sh_new_ver} != ${main_version} ]]; then
 		read -p "发现新版本 ${sh_new_ver} ，是否更新？[Y/n]" yn
